208bbac8149bcb7418d07238ca027cbda7b2d84e
Knowledgehunt1 <116151857+Knowledgehunt1@users.noreply.github.com>
2025-03-20 12:10:37 +0530

    gmail api build in springboot

---
+++ src/test/java/com/aps/GmailAPI/GmailApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps.GmailAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
